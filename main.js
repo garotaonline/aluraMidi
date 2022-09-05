5033af5207979fae22170441c6d92c867478b969
Andressa L. de Lima <96728998+garotaonline@users.noreply.github.com>
2022-09-05 16:57:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+tocaSom = (seletorAudio) => {
+    const elemento = document.querySelector(seletorAudio);
+    
+    if (elemento != null && elemento.localName === "audio") {
+        elemento.play();
+    } else {
+        alert(`Elemento não encontrado ou seletor inválido!`);
+    };
+
+};
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function (evento) {
+        if(evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }   
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    };
+
+};
